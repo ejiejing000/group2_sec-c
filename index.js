e7aa22d6efe6899bb0fcb10a6187935f4aa49543
unknown <pc11@csitlab.local>
2026-09-18 11:30:34 +0800

    addind readme

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use('/', require('./routes/users.routes'))
 app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/me', require('./routes/users.routes'))
 
+
+
 app.listen(1234, () => {
 	console.log('Server is running on http://localhost:1234')
 })
